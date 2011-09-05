0356341efb40257b0f17e1ef596358643573a5eb
olajide.dele@gmail.com <olajide.dele@gmail.com@2aca5568-8798-a1b4-05fb-33b9b96d7e1a>
2011-09-05 16:51:20 +0000

    Removed Nellymoser codec for Flash Player 11.0

--- trunk/WEB-INF/classes/org/red5/server/webapp/voicebridge/RtmpParticipant.java
+++ trunk/WEB-INF/classes/org/red5/server/webapp/voicebridge/RtmpParticipant.java
@@ -41,6 +41,10 @@ import org.red5.server.stream.IStreamData;
 import org.red5.server.stream.message.RTMPMessage;
 
 import com.sun.voip.server.MemberReceiver;
+import com.sun.voip.AudioConversion;
+
+import com.sun.voip.AudioConversion;
+
 import org.red5.codecs.asao.CodecImpl;
 
 public class RtmpParticipant extends RTMPClient implements INetStreamEventHandler, ClientExceptionHandler, IPendingServiceCallback {
@@ -61,26 +65,12 @@ public class RtmpParticipant extends RTMPClient implements INetStreamEventHandle
     private short kt2 = 0;
     private IoBuffer buffer;
 
-   	private float[] senderEncoderMap = new float[64];
-   	private float[] recieverEncoderMap = new float[64];
-
-    private static final int NELLYMOSER_CODEC_ID = 82;
-    private static final int L16_AUDIO_LENGTH = 256;
-    private static final int NELLY_AUDIO_LENGTH = 64;
+    private static final int ULAW_CODEC_ID = 130;
     private static final int ULAW_AUDIO_LENGTH = 160;
-    private static final int MAX_BUFFER_LENGTH = 1280;
 
-    private final IntBuffer l16AudioSender = IntBuffer.allocate(MAX_BUFFER_LENGTH);
-    private final IntBuffer l16AudioRecv = IntBuffer.allocate(MAX_BUFFER_LENGTH);
-
-    private IntBuffer viewBufferSender;
-    private IntBuffer viewBufferRecv;
-
-    private int[] tempNellyBuffer = new int[L16_AUDIO_LENGTH];
-    private final byte[] nellyBytes = new byte[NELLY_AUDIO_LENGTH];
-
-    private int[] tempL16Buffer = new int[L16_AUDIO_LENGTH];
-    private int[] l16Buffer = new int[ULAW_AUDIO_LENGTH];
+	private final byte[] byteData = new byte[ULAW_AUDIO_LENGTH + 1];
+	private final byte[] byteBuffer = new byte[ULAW_AUDIO_LENGTH];
+	private final int[] l16Buffer = new int[ULAW_AUDIO_LENGTH];
 
 	private long startTime = System.currentTimeMillis();
 
@@ -139,8 +129,8 @@ public class RtmpParticipant extends RTMPClient implements INetStreamEventHandle
     //
     // ------------------------------------------------------------------------
 
-    public void startStream(String host, String app, int port, String publishName, String playName, long conferenceStartTime ) {
-
+    public void startStream(String host, String app, int port, String publishName, String playName, long conferenceStartTime )
+    {
         System.out.println( "RtmpParticipant startStream" );
 
 		if (publishName == null || playName == null)
@@ -159,12 +149,9 @@ public class RtmpParticipant extends RTMPClient implements INetStreamEventHandle
 			audioTs = 0;
 			kt = 0;
 			kt2 = 0;
+         	startTime = System.currentTimeMillis();
 
-			recieverEncoderMap = new float[64];
-			senderEncoderMap = new float[64];
-
-			viewBufferSender = l16AudioSender.asReadOnlyBuffer();
-			viewBufferRecv = l16AudioRecv.asReadOnlyBuffer();
+			byteData[0] = (byte) ULAW_CODEC_ID;
 
 			try {
 				connect( host, port, app, this );
@@ -177,9 +164,12 @@ public class RtmpParticipant extends RTMPClient implements INetStreamEventHandle
     }
 
 
-    public void stopStream() {
-
+    public void stopStream()
+    {
         System.out.println( "RtmpParticipant stopStream" );
+
+		kt = 0;
+		kt2 = 0;
 
         try {
             disconnect();
@@ -248,132 +238,39 @@ public class RtmpParticipant extends RTMPClient implements INetStreamEventHandle
 
 	public void pushAudio(int[] pcmBuffer)
 	{
-		int timeStamp = 0;
-
-        if ( kt < 10 ) {
-        	System.out.println( "RtmpParticipant.pushAudio() - dataToSend -> length = " + pcmBuffer.length + ".");
-		}
-
 		try {
-			l16AudioSender.put(pcmBuffer);
+	    	AudioConversion.linearToUlaw(pcmBuffer, byteData, 1);
 
-			if ((l16AudioSender.position() - viewBufferSender.position()) >= L16_AUDIO_LENGTH)
-			{
-				// We have enough L16 audio to generate a Nelly audio.
-				// Get some L16 audio
+			int ts = (int)(System.currentTimeMillis() - startTime);
 
-				viewBufferSender.get(tempNellyBuffer);
-
-				// adjust volume
-				//normalize(tempNellyBuffer);
-
-				// Convert it into Nelly
-
-				CodecImpl.encode(senderEncoderMap, tempNellyBuffer, nellyBytes);
-
-				// Having done all of that, we now see if we need to send the audio or drop it.
-				// We have to encode to build the encoderMap so that data from previous audio packet
-				// will be used for the next packet.
-
-				boolean sendPacket = true;
-				IConnection conn = Red5.getConnectionLocal();
-
-				if (conn instanceof RTMPMinaConnection) {
-					long pendingMessages = ((RTMPMinaConnection)conn).getPendingMessages();
-
-					if (pendingMessages > 25) {
-						// Message backed up probably due to slow connection to client (25 messages * 20ms ptime = 500ms audio)
-						sendPacket = false;
-						System.out.println(String.format("Dropping packet. Connection %s congested with %s pending messages (~500ms worth of audio) .", conn.getClient().getId(), pendingMessages));
-					}
-				}
-
-				if (sendPacket) {
-
-					if (kt == 0)
-					{
-						startTime = System.currentTimeMillis();
-						timeStamp = 0;
-
-					} else {
-
-						timeStamp = (int)(System.currentTimeMillis() - startTime);
-					}
-
-					pushAudio(NELLY_AUDIO_LENGTH, nellyBytes, timeStamp, NELLYMOSER_CODEC_ID);
-				}
+			if ( buffer == null ) {
+				buffer = IoBuffer.allocate( 1024 );
+				buffer.setAutoExpand( true );
 			}
+
+			buffer.clear();
+			buffer.put( byteData );
+			buffer.flip();
+
+			AudioData audioData = new AudioData( buffer );
+			audioData.setTimestamp( ts );
+
+			kt++;
+
+			if ( kt < 10 ) {
+				loggerdebug( "+++ " + audioData );
+			}
+
+			RTMPMessage rtmpMsg = new RTMPMessage();
+			rtmpMsg.setBody( audioData );
+			publishStreamData( publishStreamId, rtmpMsg );
 
 		} catch (Exception e) {
 
             loggererror( "RtmpParticipant pushAudio exception " + e );
 		}
 
-        if (l16AudioSender.position() == l16AudioSender.capacity()) {
-        	// We've processed 8 Ulaw packets (5 Nelly packets), reset the buffers.
-        	l16AudioSender.clear();
-        	viewBufferSender.clear();
-        }
 	}
-
-    public void pushAudio( int len, byte[] audio, int ts, int codec ) throws IOException {
-
-        if ( buffer == null ) {
-            buffer = IoBuffer.allocate( 1024 );
-            buffer.setAutoExpand( true );
-        }
-
-        buffer.clear();
-
-        buffer.put( (byte) codec ); // first byte 2 mono 5500; 6 mono 11025; 22
-        // mono 11025 adpcm 82 nellymoser 8000 178
-        // speex 8000
-        buffer.put( audio );
-
-        buffer.flip();
-
-        AudioData audioData = new AudioData( buffer );
-        audioData.setTimestamp( ts );
-
-        kt++;
-
-        if ( kt < 10 ) {
-            loggerdebug( "+++ " + audioData );
-        }
-
-        RTMPMessage rtmpMsg = new RTMPMessage();
-        rtmpMsg.setBody( audioData );
-        publishStreamData( publishStreamId, rtmpMsg );
-    }
-
-
-   	public int[] normalize(int[] audio)
-   	{
-	    // Scan for max peak value here
-	    float peak = 0;
-		for (int n = 0; n < audio.length; n++)
-		{
-			int val = Math.abs(audio[n]);
-			if (val > peak)
-			{
-				peak = val;
-			}
-		}
-
-		// Peak is now the loudest point, calculate ratio
-		float r1 = 32768 / peak;
-
-		// Don't increase by over 500% to prevent loud background noise, and normalize to 75%
-		float ratio = Math.min(r1, 5) * .75f;
-
-		for (int n = 0; n < audio.length; n++)
-		{
-			audio[n] *= ratio;
-		}
-
-		return audio;
-
-   	}
 
     // ------------------------------------------------------------------------
     //
@@ -441,7 +338,8 @@ public class RtmpParticipant extends RTMPClient implements INetStreamEventHandle
         }
 
 
-        public void dispatchEvent( IEvent event ) {
+        public void dispatchEvent( IEvent event )
+        {
 
             if ( !( event instanceof IRTMPEvent ) ) {
                 loggerdebug( "skipping non rtmp event: " + event );
@@ -475,60 +373,23 @@ public class RtmpParticipant extends RTMPClient implements INetStreamEventHandle
 
                 IoBuffer audioData = ( (IStreamData) rtmpEvent ).getData().asReadOnlyBuffer();
 
-				byte[] asaoInput = new byte[audioData.limit() - 1];
 				audioData.rewind();
 				audioData.position(audioData.position() + 1);
-				audioData.get(asaoInput);
+				audioData.get(byteBuffer);
 
-                CodecImpl.decode(recieverEncoderMap, asaoInput, tempL16Buffer);
+		        AudioConversion.ulawToLinear(byteBuffer, 0, byteBuffer.length, l16Buffer);
 
-				l16AudioRecv.put(tempL16Buffer);	// Store the L16 audio into the buffer
-
-    			viewBufferRecv.get(l16Buffer);		// Read 160-int worth of audio
-				sendToBridge(l16Buffer);
-
-    			if (l16AudioRecv.position() == l16AudioRecv.capacity())
-    			{
-					/**
-					 *  This means we already processed 5 Nelly packets and sent 5 Ulaw packets.
-					 *  However, we have 3 extra Ulaw packets.
-					 *  Fire them off to the bridge. We don't want to discard them as it will
-					 *  result in choppy audio.
-					 */
-
-					for (int i=0; i<3; i++)
-					{
-						viewBufferRecv.get(l16Buffer);
-						sendToBridge(l16Buffer);
-					}
-
-					// Reset the buffer's position back to zero and start over.
-
-					l16AudioRecv.clear();
-					viewBufferRecv.clear();
-				}
-			}
-        }
-
-        private void sendToBridge(int[] encodingBuffer)
-        {
-			try {
-
-				if (memberReceiver != null ) {
-
-					memberReceiver.handleRTMPMedia(encodingBuffer, kt2);
+				if (memberReceiver != null )
+				{
+					memberReceiver.handleRTMPMedia(l16Buffer, kt2);
 
 					if ( kt2 < 10 ) {
-						loggerdebug( "*** " + encodingBuffer.length );
+						loggerdebug( "*** " + l16Buffer.length );
 					}
 
 					kt2++;
 				}
-			}
-			catch ( Exception e ) {
-				loggererror( "RtmpParticipant => sendToBridge error " + e );
-				e.printStackTrace();
-			}
-		}
+ 			}
+        }
     }
 }
