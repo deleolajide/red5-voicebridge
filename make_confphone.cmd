863c67a092f1bd5b4773639e3e3866d8c81d900c
olajide.dele <olajide.dele@gmail.com>
2011-09-05 16:51:20 +0000

    Removed Nellymoser codec for Flash Player 11.0

--- make_confphone.cmd
+++ make_confphone.cmd
@@ -1,4 +1,4 @@
-C:\Projects\flex33\bin\mxmlc.exe   confphone.mxml
+C:\Projects\flex45\bin\mxmlc.exe  confphone.mxml  -theme C:\Projects\flex45\frameworks\themes\Halo\halo.swc -swf-version=12
 
 pause
 
